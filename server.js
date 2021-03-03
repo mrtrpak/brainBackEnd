14d63216d38c31c694df34c45fab496addd139d0
Michael Trpak <mrtrpak3@gmail.com>
2021-03-03 17:56:22 -0500

    able to update entries and increment by 1

--- server.js
+++ server.js
@@ -80,18 +80,14 @@ app.get('/profile/:id', (req, res) => {
 });
 
 app.put('/image', (req, res) => {
-  const { id } = req.params;
-  let found = false;
-  // mockDB.users.forEach(user => {
-  //   if (user.id === id) {
-  //     found = true;
-  //     user.entries++
-  //     return res.json(user.entries);
-  //   }
-  // })
-  // if (!found) {
-  //   res.status(400).json('not found');
-  // };
+  const { id } = req.body;
+  
+  db('users').where('id', '=', id).increment('entries', 1)
+    .returning('entries').then(
+      entries => {
+        res.json(entries[0]);  
+      }
+    ).catch(err => res.status(400).json('unable to get entries'))
 });
 
 app.listen(3001, () => {
