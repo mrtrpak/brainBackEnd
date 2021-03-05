a8eeb46ca9ef0560d766fc4c1c7c3a08bc465824
Michael Trpak <mrtrpak3@gmail.com>
2021-03-04 20:38:38 -0500

    adding logic to check register password and email

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ const db = knex({
   }
 });
 
+const validatedEmail = email => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+};
+
 const app = express();
 
 //middleware to read json
@@ -32,7 +36,7 @@ app.post('/signin', (req, res) => {
   const { email, password } = req.body;
 
   db.select('email', 'hash').from('login')
-    .where('email', '=', email)  
+    .where('email', '=', email)
     .then(data => {
       const isValid = bcrypt.compareSync(password, data[0].hash);
 
@@ -47,36 +51,40 @@ app.post('/signin', (req, res) => {
         res.status(400).json('wrong credentials');
       }
     }
-  )
-  .catch(err => res.status(400).json('wrong credentials'));
+    )
+    .catch(err => res.status(400).json('wrong credentials'));
 });
 
 app.post('/register', (req, res) => {
   const { name, email, password } = req.body;
   let hash = bcrypt.hashSync(password);
 
-  db.transaction(trx => {
-    trx.insert({
-      hash: hash,
-      email: email
-    })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-          .returning('*')
-          .insert({
-            name: name,
-            email: loginEmail[0],
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0]);
-          })
+  if (password.length < 8 || password.length > 100 || !validatedEmail(email)) {
+    res.status(400).json('information entered incorrectly');
+  } else {
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
       })
-      .then(trx.commit).catch(trx.rollback);
-  })
-  .catch(() => res.status(400).json('unable to register'));
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+          return trx('users')
+            .returning('*')
+            .insert({
+              name: name,
+              email: loginEmail[0],
+              joined: new Date()
+            })
+            .then(user => {
+              res.json(user[0]);
+            })
+        })
+        .then(trx.commit).catch(trx.rollback);
+    })
+      .catch(() => res.status(400).json('unable to register'));
+  };
 });
 
 // Can be used for profile page to update delete
