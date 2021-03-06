44e5df0284f5ac2aaafdb547942faa4beb1faac4
Michael Trpak <mrtrpak3@gmail.com>
2021-03-06 15:31:45 -0500

    adding controllers folder and adding register too it

---
+++ controllers/register.js
@@ -0,0 +1,31 @@
+export const handleRegister = (req, res) => {
+  const { name, email, password } = req.body;
+  let hash = bcrypt.hashSync(password);
+
+  if (password.length < 8 || password.length > 100 || !validatedEmail(email)) {
+    res.status(400).json('information entered incorrectly');
+  } else {
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+          return trx('users')
+            .returning('*')
+            .insert({
+              name: name,
+              email: loginEmail[0],
+              joined: new Date()
+            })
+            .then(user => {
+              res.json(user[0]);
+            })
+        })
+        .then(trx.commit).catch(trx.rollback);
+    })
+      .catch(() => res.status(400).json('unable to register'));
+  };
+}
--- server.js
+++ server.js
@@ -55,37 +55,7 @@ app.post('/signin', (req, res) => {
     .catch(err => res.status(400).json('wrong credentials'));
 });
 
-app.post('/register', (req, res) => {
-  const { name, email, password } = req.body;
-  let hash = bcrypt.hashSync(password);
-
-  if (password.length < 8 || password.length > 100 || !validatedEmail(email)) {
-    res.status(400).json('information entered incorrectly');
-  } else {
-    db.transaction(trx => {
-      trx.insert({
-        hash: hash,
-        email: email
-      })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-          return trx('users')
-            .returning('*')
-            .insert({
-              name: name,
-              email: loginEmail[0],
-              joined: new Date()
-            })
-            .then(user => {
-              res.json(user[0]);
-            })
-        })
-        .then(trx.commit).catch(trx.rollback);
-    })
-      .catch(() => res.status(400).json('unable to register'));
-  };
-});
+app.post('/register', register.handleRegister(req, res));
 
 // Can be used for profile page to update delete
 // When object key is same as value EX: id you can just write it once
