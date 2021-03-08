23644e9acf32d2b82d5eb777715cbb0f8ed5f0b1
Michael Trpak <mrtrpak3@gmail.com>
2021-03-08 15:00:22 -0500

    adding check to be sure there is email and password info

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,7 +1,10 @@
 export const handleSignin = (db, bcrypt) => (req, res) => {
   const { email, password } = req.body;
 
-  db.select('email', 'hash').from('login')
+  if (!email || !password) {
+    return res.status(400).json('incorrect submission');
+  } else {
+    db.select('email', 'hash').from('login')
     .where('email', '=', email)
     .then(data => {
       const isValid = bcrypt.compareSync(password, data[0].hash);
@@ -13,10 +16,11 @@ export const handleSignin = (db, bcrypt) => (req, res) => {
             res.json(user[0]);
           })
           .catch(err => res.status(400).json('unable to get user'));
-      } else {
-        res.status(400).json('wrong credentials');
+        } else {
+          res.status(400).json('wrong credentials');
+        }
       }
-    }
-    )
-    .catch(err => res.status(400).json('wrong credentials'));
+      )
+      .catch(err => res.status(400).json('wrong credentials'));
+  };
 };
