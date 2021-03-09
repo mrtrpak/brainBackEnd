213e7a4c12b0c3fcd7eb81be078d55203670b2a5
Michael Trpak <mrtrpak3@gmail.com>
2021-03-09 17:10:08 -0500

    getting info for api

--- controllers/image.js
+++ controllers/image.js
@@ -6,7 +6,12 @@ const app = new Clarifai.App({
 });
 
 export const handleApiCall = (req, res) => {
-  app.models.predict( Clarifai.FACE_DETECT_MODEL, req.body.input);
+  app.models
+    .predict( Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('api not working properly'));
 };
 
 export const handleImageGet = db => (req, res) => {
