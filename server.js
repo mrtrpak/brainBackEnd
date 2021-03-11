cb6931fa0640dcc9919491debb60a74736dfb5f0
Michael Trpak <mrtrpak3@gmail.com>
2021-03-11 16:54:33 -0500

    updates

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3001;
 const db = knex({
   client: 'pg',
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true
     // host: host,
     // user: user,
